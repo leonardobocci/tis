5b16cfcb8a586fecc7c1e7bb17beb0c8f0b7c222
leo <leonardobocci99@hotmail.com>
2024-06-24 16:37:17 +0200

    update images

--- src/app/page.js
+++ src/app/page.js
@@ -9,10 +9,10 @@ export default function Home(){
     "2.jpeg",
     "3.jpeg",
     "4.jpeg",
-    "band.jpeg",
-    "2.jpeg",
-    "3.jpeg",
-    "4.jpeg",
+    "5.jpeg",
+    "6.jpeg",
+    "7.jpeg",
+    "8.jpeg",
   ];
 
   const soundcloudTracks = [
